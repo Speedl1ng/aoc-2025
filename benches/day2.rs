379148d6330b2ee9228f0b0820eeb0785754e79e
Marcel Witoschek <92092148+Marciland@users.noreply.github.com>
2025-12-02 17:05:45 +0100

    Day 2 (#3)
    * star 1 done
    
    * star 2 done

--- benches/day2.rs
+++ benches/day2.rs
@@ -1,19 +1,79 @@
+use aoc_2025::read_input;
 use criterion::{Criterion, criterion_group, criterion_main};
 
-const fn part1() {
-    //
+fn part1(input: &str) -> u64 {
+    let mut result = 0;
+
+    for range in input.split(',') {
+        let (first, last): (u64, u64) = range
+            .split_once('-')
+            .map(|(f, l)| (f.parse().unwrap(), l.parse().unwrap()))
+            .unwrap();
+
+        result += (first..=last)
+            .filter(|id| {
+                let id_str = id.to_string();
+
+                let (left, right) = id_str.split_at(id_str.len() / 2);
+
+                left == right
+            })
+            .sum::<u64>();
+    }
+
+    result
 }
 
-const fn part2() {
-    //
+fn part2(input: &str) -> u64 {
+    let mut result: u64 = 0;
+
+    for range in input.split(',') {
+        let (first, last): (u64, u64) = range
+            .split_once('-')
+            .map(|(f, l)| (f.parse().unwrap(), l.parse().unwrap()))
+            .unwrap();
+
+        result += (first..=last)
+            .filter(|id| {
+                let id_str = id.to_string();
+
+                for size in 1..=(id_str.len() / 2) {
+                    let mut chunks = id_str.as_bytes().chunks(size);
+                    if chunks
+                        .next()
+                        .map(|first| chunks.all(|c| c == first))
+                        .unwrap()
+                    {
+                        return true;
+                    }
+                }
+
+                false
+            })
+            .sum::<u64>();
+    }
+
+    result
 }
 
 fn bench_part1(c: &mut Criterion) {
-    c.bench_function("day2 part1", |b| b.iter(part1));
+    let input = read_input(2);
+    let input = input.join("");
+
+    let result = part1(&input);
+    println!("day2 part1 result: {result}");
+
+    c.bench_function("day2 part1", |b| b.iter(|| part1(&input)));
 }
 
 fn bench_part2(c: &mut Criterion) {
-    c.bench_function("day2 part2", |b| b.iter(part2));
+    let input = read_input(2);
+    let input = input.join("");
+
+    let result = part2(&input);
+    println!("day2 part2 result: {result}");
+
+    c.bench_function("day2 part2", |b| b.iter(|| part2(&input)));
 }
 
 criterion_group!(benches, bench_part1, bench_part2);
